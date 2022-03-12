90405047bdf4de5d23fef34af58ddf12cc4fd978
jvduarte <victorduarte.ufrj@gmail.com>
2022-03-12 18:15:09 -0300

    contacts

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Contato 
+
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
